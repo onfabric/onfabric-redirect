15f2f11c7f686b3a1c94a80f7a9f8e11e42c94cf
Andy Clifford <andy@onfabric.io>
2025-01-14 15:24:53 +1300

    Create Next.js App

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # onfabric-redirect
+
 A demonstration of how to create a link user &amp; token then trigger the consent flow
+
+## Quick Start
+
+First, run the development server:
+
+```bash
+npm install
+
+npm run dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser.
---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "onfabric | redirect demonstration",
+  description: "onfabric's demonstration of a redirect to the consent flow",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
