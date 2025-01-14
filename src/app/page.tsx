0b885470f552eecc1f7fb1490c669030631b9ddd
Andy Clifford <andy@onfabric.io>
2025-01-15 11:10:56 +1300

    Test for in app browsers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,32 @@
+"use client"
+
+import { useEffect, useState } from 'react';
+
 export default function Home() {
-  return <></>
+  const [userAgent, setUserAgent] = useState('');
+  const [inApp, setInApp] = useState(false);
+
+  useEffect(() => {
+    const ua = navigator.userAgent;
+    setUserAgent(ua);
+
+    const rules = [
+      'WebView',
+      '(iPhone|iPod|iPad)(?!.*Safari\\/)',
+      'Android.*(wv)',
+    ];
+    const regex = new RegExp(`(${rules.join('|')})`, 'ig');
+    setInApp(Boolean(ua.match(regex)));
+  }, []);
+
+  return (
+    <div>  
+      <div>
+        User Agent: {userAgent || 'Loading...'}
+      </div>
+      <div>
+        In App: {inApp ? 'true' : 'false'}
+      </div>
+    </div>
+  );
 }
