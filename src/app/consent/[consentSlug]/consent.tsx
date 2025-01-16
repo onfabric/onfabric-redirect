cf8bb6147f2ff7df4510bda93307216c0a139a01
Andy Clifford <andy@onfabric.io>
2025-01-16 15:30:44 +1300

    Implement form to capture users email after consent flow

--- src/app/consent/[consentSlug]/consent.tsx
+++ src/app/consent/[consentSlug]/consent.tsx
@@ -28,17 +28,17 @@ export default function Consent(props: { consentSlug: string, userId?: string })
         window.location.replace(`${process.env.NEXT_PUBLIC_CONSENT_APP_URL}/consent/${props.consentSlug}?token=${response.token}`);
       });
     }
-  }, [inAppBrowser]);
+  }, [inAppBrowser, props.consentSlug, props.userId]);
 
   return (
-    <div className="flex flex-col items-center text-center h-screen max-w-xl m-auto pt-16 pb-4 px-4">
+    <div className="flex flex-col items-center h-screen max-w-xl m-auto pt-16 pb-4 px-6">
       <Avatar>
         <AvatarImage src="/logo-icon-dark.svg" alt="Fabric Logo"/>
       </Avatar>
-      <h1 className="mt-12 text-2xl font-bold">
+      <h1 className="mt-12 text-2xl font-bold text-center">
         { inAppBrowser ? "In-app Browser Detected" : "Redirecting..." }
       </h1>
-      <p className="px-8 mt-4">
+      <p className="px-8 mt-4 text-center">
         { inAppBrowser ? "We’ve detected that you’re using an in-app browser. For the best experience, please open the link in your default browser." : "We will redirect you through our consent flow shortly." }
       </p>
     </div>
